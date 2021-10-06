40a0ff10287cb3196ce806c0040932c33ff0079b
IvanLink-py <ostaninivan8@gmail.com>
2021-10-06 21:49:56 +0500

    Моделирование реакторной + установка Unity Particle Pack

--- Assets/Scripts/rotor.cs
+++ Assets/Scripts/rotor.cs
@@ -5,10 +5,11 @@ using UnityEngine;
 public class rotor : MonoBehaviour
 {
     public float speed = 15.0f;
+    public Vector3 up = Vector3.up;
 
     // Update is called once per frame
     void Update()
     {
-        transform.RotateAround(transform.position, Vector3.up, speed);
+        transform.RotateAround(transform.position, up, speed);
     }
 }
